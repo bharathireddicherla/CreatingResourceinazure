b1be5bf87facce9e73df4680978793d11ea27810
bharathireddicherla <68938765+bharathireddicherla@users.noreply.github.com>
2021-05-17 15:45:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "rg" {
 
-  name     = "JenkinsIntegration"
+  name     = "JenkinsIntegration2"
   location = "North Europe"
   tags = {
         Environment = "Terraform Getting Started"
